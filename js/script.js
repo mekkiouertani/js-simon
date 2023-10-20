d609b29ddf7c7b0abcba9f1cf39ef57488071121
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-20 15:19:54 +0200

    add comments to js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-//
+//aggiungere una struttura su html
+//aggiungere un bottone che fa comparire i 5 numeri casuali
+//dopo che si clicca sul bottone START compariranno i numeri per 30 secondi
+//alla fine dei 30 secondi i numeri compariranno
+//e comparirà la casella di input
+//l'utente inserisce i numeri che si ricorda
+//mostriamo il messaggio finale
